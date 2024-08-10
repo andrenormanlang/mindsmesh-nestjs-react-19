5c54437b7ba114715a5474a3de4af6c39c852665
andrenormanlang <andrernormanlang@gmail.com>
2024-08-10 08:12:16 +0200

    cleaned app module code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,23 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-console.log({
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
