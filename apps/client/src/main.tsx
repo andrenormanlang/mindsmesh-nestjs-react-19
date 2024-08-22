c51c4ef34528a8fcf246b26955e13c923c8ca15a
andrenormanlang <andrernormanlang@gmail.com>
2024-08-22 09:00:36 +0200

    fixed css file

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./app.css";
+import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
