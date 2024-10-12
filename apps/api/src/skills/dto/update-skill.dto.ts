60112684e74c4c556b396e5c055d8fe11cda44ba
andrenormanlang <andrenormanlang@gmail.com>
2024-10-12 21:05:02 +0200

    updating lock file and adding vercel.json to client for deployment

--- apps/api/src/skills/dto/update-skill.dto.ts
+++ apps/api/src/skills/dto/update-skill.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateSkillDto } from './create-skill.dto';
 
-export class UpdateSkillDto extends PartialType(CreateSkillDto) {}
+export class UpdateSkillDto extends PartialType(CreateSkillDto) {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    isAvailable: boolean; 
+}
